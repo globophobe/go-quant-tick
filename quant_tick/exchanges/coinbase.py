b442ee5afd513055df9ee57bc9ac21558f02ca13
Alex <globophobe@gmail.com>
2025-09-15 18:18:43 +0900

    :necktie: current exchanges only

--- quant_tick/exchanges/coinbase.py
+++ quant_tick/exchanges/coinbase.py
@@ -1,13 +1,97 @@
+from collections import defaultdict
 from decimal import Decimal
 from typing import Any
 
-from cryptofeed.defines import ASK, BID, L3_BOOK, SELL, TRADES
+from cryptofeed.connection import (
+    AsyncConnection,
+    RestEndpoint,
+    Routes,
+    WebsocketEndpoint,
+)
+from cryptofeed.defines import ASK, BID, BUY, L3_BOOK, SELL, TRADES
 from cryptofeed.exchanges import Coinbase as BaseCoinbase
+from cryptofeed.symbols import Symbol
+from cryptofeed.types import Trade
+from yapic import json
 
 from ..feed import Feed
 
 
 class Coinbase(Feed, BaseCoinbase):
+    """Coinbase."""
+
+    websocket_endpoints = [
+        WebsocketEndpoint(
+            "wss://ws-feed.exchange.coinbase.com", options={"compression": None}
+        )
+    ]
+    rest_endpoints = [
+        RestEndpoint("https://api.exchange.coinbase.com", routes=Routes("/products"))
+    ]
+
+    @classmethod
+    def _parse_symbol_data(cls, data: list) -> tuple[dict, dict]:
+        """Parse symbol data."""
+        ret = {}
+        info = defaultdict(dict)
+
+        for entry in data:
+            sym = Symbol(entry["base_currency"], entry["quote_currency"])
+            info["tick_size"][sym.normalized] = entry["quote_increment"]
+            info["instrument_type"][sym.normalized] = sym.type
+            ret[sym.normalized] = entry["id"]
+        return ret, info
+
+    async def subscribe(self, conn: AsyncConnection) -> None:
+        """Subscribe."""
+        self.__reset()
+        all_pairs = list()
+
+        async def _subscribe(chan: str, product_ids: list) -> None:
+            """Subscribe."""
+            if chan == "market_trades":
+                chan = {"name": "ticker", "product_ids": product_ids}
+
+            params = {"type": "subscribe", "product_ids": product_ids, "channels": chan}
+            await conn.write(json.dumps(params))
+
+        for channel in self.subscription:
+            all_pairs += self.subscription[channel]
+            await _subscribe(channel, self.subscription[channel])
+        all_pairs = list(dict.fromkeys(all_pairs))
+        await _subscribe("heartbeat", all_pairs)
+        # Implementing heartbeat as per Best Practices doc: https://docs.cloud.coinbase.com/advanced-trade-api/docs/ws-best-practices
+
+    async def _trade_update(self, msg: dict, timestamp: float):
+        """
+        {
+            'trade_id': 43736593
+            'side': 'BUY' or 'SELL',
+            'size': '0.01235647',
+            'price': '8506.26000000',
+            'product_id': 'BTC-USD',
+            'time': '2018-05-21T00:26:05.585000Z'
+        }
+        """
+        pair = self.exchange_symbol_to_std_symbol(msg["product_id"])
+        ts = self.timestamp_normalize(msg["time"])
+        order_type = "market"
+        t = Trade(
+            self.id,
+            pair,
+            SELL if msg["side"] == "SELL" else BUY,
+            Decimal(msg["size"]),
+            Decimal(msg["price"]),
+            ts,
+            id=str(msg["trade_id"]),
+            type=order_type,
+            raw=msg,
+        )
+        import pdb
+
+        pdb.set_trace()
+        await self.callback(TRADES, t, timestamp)
+
     async def _book_update(self, msg: dict, timestamp: float) -> Any:
         """
         {
@@ -74,4 +158,7 @@ class Coinbase(Feed, BaseCoinbase):
             "notional": notional,
             "tickRule": 1 if msg["side"].lower() == SELL else -1,
         }
+        import pdb
+
+        pdb.set_trace()
         await self.callback(TRADES, t, ts)
--- quant_tick/trades/__init__.py
+++ quant_tick/trades/__init__.py
@@ -1,5 +1,4 @@
 from .candles import CandleCallback
-from .trade_cluster import TradeClusterCallback
 from .significant_trades import SignificantTradeCallback
 from .trades import (
     NonSequentialIntegerTradeCallback,
@@ -7,17 +6,9 @@ from .trades import (
     TradeCallback,
 )
 
-# Optional
-try:
-    from .gcppubsub import GCPPubSubTradeCallback
-except ImportError:
-    pass
-
 __all__ = [
-    "GCPPubSubTradeCallback",
     "CandleCallback",
     "TradeCallback",
-    "TradeClusterCallback",
     "SignificantTradeCallback",
     "SequentialIntegerTradeCallback",
     "NonSequentialIntegerTradeCallback",
