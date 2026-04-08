4c81a2063141840ae3e0a7ab1fdd3015fe60c4d3
Alex <globophobe@gmail.com>
2026-04-09 07:44:59 +0900

    :fire: delete unnecessary code

--- quant_tick/trades/__init__.py
+++ quant_tick/trades/__init__.py
@@ -1,15 +1,7 @@
-from .candles import CandleCallback
 from .significant_trades import SignificantTradeCallback
-from .trades import (
-    NonSequentialIntegerTradeCallback,
-    SequentialIntegerTradeCallback,
-    TradeCallback,
-)
+from .trades import TradeCallback
 
 __all__ = [
-    "CandleCallback",
     "TradeCallback",
     "SignificantTradeCallback",
-    "SequentialIntegerTradeCallback",
-    "NonSequentialIntegerTradeCallback",
 ]
--- quant_tick/trades/utils.py
+++
@@ -1,4 +0,0 @@
-def normalize_symbol(feed: str, symbol: str) -> str:
-    for char in ("-", "/", "_"):
-        symbol = symbol.replace(char, "")
-    return symbol
